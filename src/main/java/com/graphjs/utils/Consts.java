29e023b1300b0ffc638dd071fd0a288e1a81aad8
ambou-97 <ambroise.tine@gmail.com>
2022-09-06 22:59:32 +0100

    update

---
+++ src/main/java/com/graphjs/utils/Consts.java
@@ -0,0 +1,7 @@
+package com.graphjs.utils;
+
+public class Consts {
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String FRONT_TUTORIAL_LINK = "#";
+    public static final int WISHES_LIMIT = 7;
+}
