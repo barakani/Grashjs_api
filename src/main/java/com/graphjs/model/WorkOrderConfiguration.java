3bfb48715524d0f511d41b6af7a6515bb837bdf0
ambou-97 <ambroise.tine@gmail.com>
2022-09-08 23:53:11 +0100

    update models

---
+++ src/main/java/com/graphjs/model/WorkOrderConfiguration.java
@@ -0,0 +1,21 @@
+package com.graphjs.model;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class WorkOrderConfiguration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+//    private FieldConfiguration fieldConfiguration;
+
+    private boolean isCreation;
+}
