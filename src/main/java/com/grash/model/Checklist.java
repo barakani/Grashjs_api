2bfc38dd6f2feb4ec894e881a3a238db62faf7d2
ambou-97 <ambroise.tine@gmail.com>
2022-09-10 17:37:45 +0100

    Checklist mapping

--- src/main/java/com/grash/model/Checklist.java
+++ src/main/java/com/grash/model/Checklist.java
@@ -3,10 +3,8 @@ package com.grash.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 @Data
@@ -16,7 +14,8 @@ public class Checklist {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-//    private List<TaskBase> taskBaseList;
+    @OneToMany
+    private Collection<TaskBase> taskBaseList;
 
     private String name;
 
