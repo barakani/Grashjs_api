e7c9ffcf33a3d4853f80883a33f9287c49618783
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-11 15:26:05 +0100

    feat: ModelMapper

--- src/main/java/com/grash/ApiApplication.java
+++ src/main/java/com/grash/ApiApplication.java
@@ -2,6 +2,8 @@ package com.grash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +12,8 @@ public class ApiApplication {
         SpringApplication.run(ApiApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
--- src/main/java/com/grash/model/enums/RoleType.java
+++ src/main/java/com/grash/model/enums/RoleType.java
@@ -3,7 +3,7 @@ package com.grash.model.enums;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum RoleType implements GrantedAuthority {
-    ADMIN,
+    SUPER_ADMIN,
     CLIENT;
 
     public String getAuthority() {
