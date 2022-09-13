1460fd631897b66f89a86c1b9a6885c8179be957
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-14 00:41:16 +0100

    fix: AssetCategoryController

--- src/main/java/com/grash/controller/AssetCategoryController.java
+++ src/main/java/com/grash/controller/AssetCategoryController.java
@@ -1,12 +1,13 @@
 package com.grash.controller;
 
+import com.grash.dto.SuccessResponse;
 import com.grash.exception.CustomException;
 import com.grash.model.AssetCategory;
 import com.grash.model.Company;
 import com.grash.model.CompanySettings;
 import com.grash.model.User;
 import com.grash.service.AssetCategoryService;
-import com.grash.service.CompanyService;
+import com.grash.service.CompanySettingsService;
 import com.grash.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiParam;
@@ -14,6 +15,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +29,7 @@ import java.util.Optional;
 public class AssetCategoryController {
 
     private final AssetCategoryService assetCategoryService;
+    private final CompanySettingsService companySettingsService;
     private final UserService userService;
 
     @GetMapping("/{id}")
@@ -37,12 +40,9 @@ public class AssetCategoryController {
             @ApiResponse(code = 404, message = "AssetCategory not found")})
     public Optional<AssetCategory> getById(@ApiParam("id") @PathVariable("id") Long id, HttpServletRequest req) {
         User user = userService.whoami(req);
-        Company company = user.getCompany();
-
         Optional<AssetCategory> optionalAssetCategory = assetCategoryService.findById(id);
-
         if (optionalAssetCategory.isPresent()) {
-            if (userService.findById(optionalAssetCategory.get().getCreatedBy()).get().getCompany().getId().equals(company)){
+            if (hasAccess(user, optionalAssetCategory.get())) {
                 return assetCategoryService.findById(id);
             } else {
                 throw new CustomException("Can't get assetCategory from other company", HttpStatus.NOT_ACCEPTABLE);
@@ -52,6 +52,22 @@ public class AssetCategoryController {
         }
     }
 
+    @PostMapping("")
+    @PreAuthorize("permitAll()")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied")})
+    public AssetCategory create(@ApiParam("AssetCategory") @RequestBody AssetCategory assetCategoryReq, HttpServletRequest req) {
+        User user = userService.whoami(req);
+        Optional<CompanySettings> optionalCompanySettings = companySettingsService.findById(assetCategoryReq.getCompanySettings().getId());
+        if (optionalCompanySettings.isPresent()) {
+            Company company = optionalCompanySettings.get().getCompany();
+            if (user.getCompany().getId().equals(company.getId())) {
+                return assetCategoryService.create(assetCategoryReq);
+            } else throw new CustomException("Access denied", HttpStatus.FORBIDDEN);
+        } else throw new CustomException("Invalid CompanySettings", HttpStatus.NOT_ACCEPTABLE);
+    }
+
     @PatchMapping("/{id}")
     @PreAuthorize("permitAll()")
     @ApiResponses(value = {//
@@ -59,16 +75,14 @@ public class AssetCategoryController {
             @ApiResponse(code = 403, message = "Access denied"), //
             @ApiResponse(code = 404, message = "AssetCategory not found")})
     public AssetCategory patch(@ApiParam("AssetCategory") @RequestBody AssetCategory assetCategory,
-                                 @ApiParam("id") @PathVariable("id") Long id,
+                               @ApiParam("id") @PathVariable("id") Long id,
                                HttpServletRequest req) {
         User user = userService.whoami(req);
-        Company company = user.getCompany();
-
         Optional<AssetCategory> optionalAssetCategory = assetCategoryService.findById(id);
 
         if (optionalAssetCategory.isPresent()) {
             AssetCategory savedAssetCategory = optionalAssetCategory.get();
-            if (userService.findById(optionalAssetCategory.get().getCreatedBy()).get().getCompany().getId().equals(company)){
+            if (hasAccess(user, savedAssetCategory)) {
                 return assetCategoryService.update(assetCategory);
             } else {
                 throw new CustomException("Can't patch assetCategory from other company", HttpStatus.NOT_ACCEPTABLE);
@@ -78,6 +92,30 @@ public class AssetCategoryController {
         }
 
 
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("permitAll()")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied"), //
+            @ApiResponse(code = 404, message = "AssetCategory not found")})
+    public ResponseEntity delete(@ApiParam("id") @PathVariable("id") Long id, HttpServletRequest req) {
+        User user = userService.whoami(req);
+
+        Optional<AssetCategory> optionalAssetCategory = assetCategoryService.findById(id);
+        if (optionalAssetCategory.isPresent()) {
+            if (hasAccess(user, optionalAssetCategory.get())) {
+                assetCategoryService.delete(id);
+                return new ResponseEntity(new SuccessResponse(true, "Deleted successfully"),
+                        HttpStatus.OK);
+            } else throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
+        } else throw new CustomException("AssetCategory not found", HttpStatus.NOT_FOUND);
+    }
+
+
+    private boolean hasAccess(User user, AssetCategory assetCategory) {
+        return userService.findById(assetCategory.getCreatedBy()).get().getCompany().getId().equals(user.getCompany().getId());
     }
 
 }
