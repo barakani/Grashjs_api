33b9bdad429828232450bcdc94f5b3163997efd2
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-14 12:14:31 +0100

    feat: CategoryPatchDTO

--- src/main/java/com/grash/service/AssetCategoryService.java
+++ src/main/java/com/grash/service/AssetCategoryService.java
@@ -1,9 +1,13 @@
 package com.grash.service;
 
+import com.grash.dto.CategoryPatchDTO;
+import com.grash.exception.CustomException;
 import com.grash.model.AssetCategory;
 import com.grash.repository.AssetCategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -20,13 +24,26 @@ public class AssetCategoryService {
         return assetCategoryRepository.save(AssetCategory);
     }
 
-    public AssetCategory update(AssetCategory AssetCategory) {
-        return assetCategoryRepository.save(AssetCategory);
+    public AssetCategory update(Long id, CategoryPatchDTO assetCategory) {
+        if (assetCategoryRepository.existsById(id)) {
+            AssetCategory savedAssetCategory = assetCategoryRepository.findById(id).get();
+            modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+            modelMapper.map(assetCategory, savedAssetCategory);
+            return assetCategoryRepository.save(savedAssetCategory);
+        } else throw new CustomException("Not found", HttpStatus.NOT_FOUND);
     }
 
-    public Collection<AssetCategory> getAll() { return assetCategoryRepository.findAll(); }
+    public Collection<AssetCategory> getAll() {
+        return assetCategoryRepository.findAll();
+    }
 
-    public void delete(Long id){ assetCategoryRepository.deleteById(id);}
+    public void delete(Long id) {
+        assetCategoryRepository.deleteById(id);
+    }
 
-    public Optional<AssetCategory> findById(Long id) {return assetCategoryRepository.findById(id); }
+    public Optional<AssetCategory> findById(Long id) {
+        return assetCategoryRepository.findById(id);
+    }
+
+
 }
