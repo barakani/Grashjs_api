225f828cd0b30547270646af818ab945b3378642
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-16 23:35:09 +0100

    refactor: optimize imports

--- src/main/java/com/grash/dto/GeneralPreferencesDTO.java
+++ src/main/java/com/grash/dto/GeneralPreferencesDTO.java
@@ -1,17 +1,12 @@
 package com.grash.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.grash.model.CompanySettings;
 import com.grash.model.Currency;
 import com.grash.model.enums.BusinessType;
-import com.grash.model.enums.DateFormat;
 import com.grash.model.enums.Language;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
--- src/main/java/com/grash/dto/LaborCostPatchDTO.java
+++ src/main/java/com/grash/dto/LaborCostPatchDTO.java
@@ -3,8 +3,6 @@ package com.grash.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-
 @Data
 @NoArgsConstructor
 public class LaborCostPatchDTO {
