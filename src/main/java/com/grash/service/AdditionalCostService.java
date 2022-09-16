061af31e7b45e6e1c74dd5df17b7a72755dd34e7
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-16 13:40:10 +0100

    chore: AdditionalTimeService

--- src/main/java/com/grash/service/AdditionalCostService.java
+++ src/main/java/com/grash/service/AdditionalCostService.java
@@ -4,8 +4,12 @@ import com.grash.dto.AdditionalCostPatchDTO;
 import com.grash.exception.CustomException;
 import com.grash.mapper.AdditionalCostMapper;
 import com.grash.model.AdditionalCost;
+import com.grash.model.Company;
+import com.grash.model.User;
+import com.grash.model.enums.RoleType;
 import com.grash.repository.AdditionalCostRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +21,10 @@ import java.util.Optional;
 public class AdditionalCostService {
 
     private final AdditionalCostRepository additionalCostRepository;
+    private final CompanyService companyService;
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
 
     private final AdditionalCostMapper additionalCostMapper;
 
@@ -43,5 +51,34 @@ public class AdditionalCostService {
         return additionalCostRepository.findById(id);
     }
 
+    public boolean hasAccess(User user, AdditionalCost additionalCost) {
+        if (user.getRole().getRoleType().equals(RoleType.ROLE_SUPER_ADMIN)) {
+            return true;
+        } else return user.getCompany().getId().equals(additionalCost.getCompany().getId());
+    }
+
+    public boolean canCreate(User user, AdditionalCost additionalCostReq) {
+        Long companyId = user.getCompany().getId();
+
+        Optional<Company> optionalCompany = companyService.findById(additionalCostReq.getCompany().getId());
+
+        //@NotNull fields
+        boolean first = optionalCompany.isPresent() && optionalCompany.get().getId().equals(companyId);
+
+        if (first && canPatch(user, modelMapper.map(additionalCostReq, AdditionalCostPatchDTO.class))) {
+            return true;
+        } else throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
+    }
+
+    public boolean canPatch(User user, AdditionalCostPatchDTO additionalCostReq) {
+        Long companyId = user.getCompany().getId();
+        Optional<User> optionalUser = additionalCostReq.getAssignedTo() == null ? Optional.empty() : userService.findById(additionalCostReq.getAssignedTo().getId());
+
+        boolean first = !optionalUser.isPresent() || optionalUser.get().getCompany().getId().equals(companyId);
+        
+        if (first) {
+            return true;
+        } else throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
+    }
 
 }
--- src/main/java/com/grash/service/AdditionalTimeService.java
+++ src/main/java/com/grash/service/AdditionalTimeService.java
@@ -2,7 +2,8 @@ package com.grash.service;
 
 import com.grash.dto.AdditionalTimePatchDTO;
 import com.grash.exception.CustomException;
-import com.grash.model.AdditionalTime;
+import com.grash.model.*;
+import com.grash.model.enums.RoleType;
 import com.grash.repository.AdditionalTimeRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.Conditions;
@@ -19,6 +20,10 @@ public class AdditionalTimeService {
     private final AdditionalTimeRepository additionalTimeRepository;
 
     private final ModelMapper modelMapper;
+    private final CompanyService companyService;
+    private final TimeCategoryService timeCategoryService;
+    private final UserService userService;
+    private final WorkOrderService workOrderService;
 
     public AdditionalTime create(AdditionalTime AdditionalTime) {
         return additionalTimeRepository.save(AdditionalTime);
@@ -43,5 +48,41 @@ public class AdditionalTimeService {
 
     public Optional<AdditionalTime> findById(Long id) {
         return additionalTimeRepository.findById(id);
+    }
+
+    public boolean hasAccess(User user, AdditionalTime additionalTime) {
+        if (user.getRole().getRoleType().equals(RoleType.ROLE_SUPER_ADMIN)) {
+            return true;
+        } else return user.getCompany().getId().equals(additionalTime.getCompany().getId());
+    }
+
+    public boolean canCreate(User user, AdditionalTime additionalTimeReq) {
+        Long companyId = user.getCompany().getId();
+
+        Optional<Company> optionalCompany = companyService.findById(additionalTimeReq.getCompany().getId());
+        Optional<WorkOrder> optionalWorkOrder = workOrderService.findById(additionalTimeReq.getWorkOrder().getId());
+
+        //@NotNull fields
+        boolean first = optionalCompany.isPresent() && optionalCompany.get().getId().equals(companyId);
+        boolean second = optionalWorkOrder.isPresent() && optionalWorkOrder.get().getCompany().getId().equals(companyId);
+
+        if (first && second && canPatch(user, modelMapper.map(additionalTimeReq, AdditionalTimePatchDTO.class))) {
+            return true;
+        } else throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
+    }
+
+    public boolean canPatch(User user, AdditionalTimePatchDTO additionalTimeReq) {
+        Long companyId = user.getCompany().getId();
+
+        Optional<TimeCategory> optionalTimeCategory = additionalTimeReq.getTimeCategory() == null ? Optional.empty() : timeCategoryService.findById(additionalTimeReq.getTimeCategory().getId());
+        Optional<User> optionalUser = additionalTimeReq.getAssignedTo() == null ? Optional.empty() : userService.findById(additionalTimeReq.getAssignedTo().getId());
+
+        //optional fields
+        boolean third = !optionalTimeCategory.isPresent() || optionalTimeCategory.get().getCompanySettings().getCompany().getId().equals(companyId);
+        boolean sixth = !optionalUser.isPresent() || optionalUser.get().getCompany().getId().equals(companyId);
+
+        if (third && sixth) {
+            return true;
+        } else throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
     }
 }
