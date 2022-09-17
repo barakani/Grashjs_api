5dd6d98ca88a65afbb3e41378dd26409ea3ebc0a
ambou-97 <ambroise.tine@gmail.com>
2022-09-17 16:56:35 +0100

    chore: RelationController

---
+++ src/main/java/com/grash/controller/RelationController.java
@@ -0,0 +1,121 @@
+package com.grash.controller;
+
+import com.grash.dto.RelationPatchDTO;
+import com.grash.dto.SuccessResponse;
+import com.grash.exception.CustomException;
+import com.grash.model.Relation;
+import com.grash.model.User;
+import com.grash.service.RelationService;
+import com.grash.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/relations")
+@Api(tags = "relation")
+@RequiredArgsConstructor
+public class RelationController {
+
+    private final RelationService relationService;
+    private final UserService userService;
+
+
+    @GetMapping("")
+    @PreAuthorize("permitAll()")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Relation not found")})
+    public Collection<Relation> getAll(HttpServletRequest req) {
+        User user = userService.whoami(req);
+        Long companyId = user.getCompany().getId();
+        return relationService.findByCompany(companyId);
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("permitAll()")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Relation not found")})
+    public Relation getById(@ApiParam("id") @PathVariable("id") Long id, HttpServletRequest req) {
+        User user = userService.whoami(req);
+        Optional<Relation> optionalRelation = relationService.findById(id);
+        if (optionalRelation.isPresent()) {
+            if (relationService.hasAccess(user, optionalRelation.get())) {
+                return relationService.findById(id).get();
+            } else {
+                throw new CustomException("Can't get relation from other company", HttpStatus.NOT_ACCEPTABLE);
+            }
+        } else throw new CustomException("Not found", HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied")})
+    public Relation create(@ApiParam("Relation") @RequestBody Relation relationReq, HttpServletRequest req) {
+        User user = userService.whoami(req);
+        if (relationService.canCreate(user, relationReq)) {
+            return relationService.create(relationReq);
+        } else throw new CustomException("Access denied", HttpStatus.FORBIDDEN);
+    }
+
+    @PatchMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied"), //
+            @ApiResponse(code = 404, message = "Relation not found")})
+    public Relation patch(@ApiParam("Relation") @RequestBody RelationPatchDTO relation,
+                          @ApiParam("id") @PathVariable("id") Long id,
+                          HttpServletRequest req) {
+        User user = userService.whoami(req);
+        Optional<Relation> optionalRelation = relationService.findById(id);
+
+        if (optionalRelation.isPresent()) {
+            Relation savedRelation = optionalRelation.get();
+            if (relationService.hasAccess(user, savedRelation) && relationService.canPatch(user, relation)) {
+                return relationService.update(id, relation);
+            } else {
+                throw new CustomException("Can't patch relation from other company", HttpStatus.NOT_ACCEPTABLE);
+            }
+        } else {
+            return null;
+        }
+
+
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied"), //
+            @ApiResponse(code = 404, message = "Relation not found")})
+    public ResponseEntity delete(@ApiParam("id") @PathVariable("id") Long id, HttpServletRequest req) {
+        User user = userService.whoami(req);
+
+        Optional<Relation> optionalRelation = relationService.findById(id);
+        if (optionalRelation.isPresent()) {
+            if (relationService.hasAccess(user, optionalRelation.get())) {
+                relationService.delete(id);
+                return new ResponseEntity(new SuccessResponse(true, "Deleted successfully"),
+                        HttpStatus.OK);
+            } else throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
+        } else throw new CustomException("Relation not found", HttpStatus.NOT_FOUND);
+    }
+
+}
--- src/main/java/com/grash/service/RelationService.java
+++ src/main/java/com/grash/service/RelationService.java
@@ -1,8 +1,16 @@
 package com.grash.service;
 
+import com.grash.dto.RelationPatchDTO;
+import com.grash.exception.CustomException;
 import com.grash.model.Relation;
+import com.grash.model.User;
+import com.grash.model.WorkOrder;
+import com.grash.model.enums.RoleType;
 import com.grash.repository.RelationRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -12,13 +20,20 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class RelationService {
     private final RelationRepository relationRepository;
+    private final WorkOrderService workOrderService;
+    private final ModelMapper modelMapper;
 
     public Relation create(Relation Relation) {
         return relationRepository.save(Relation);
     }
 
-    public Relation update(Relation Relation) {
-        return relationRepository.save(Relation);
+    public Relation update(Long id, RelationPatchDTO relation) {
+        if (relationRepository.existsById(id)) {
+            Relation savedRelation = relationRepository.findById(id).get();
+            modelMapper.getConfiguration().setPropertyCondition(Conditions.isNotNull());
+            modelMapper.map(relation, savedRelation);
+            return relationRepository.save(savedRelation);
+        } else throw new CustomException("Not found", HttpStatus.NOT_FOUND);
     }
 
     public Collection<Relation> getAll() { return relationRepository.findAll(); }
@@ -26,4 +41,32 @@ public class RelationService {
     public void delete(Long id){ relationRepository.deleteById(id);}
 
     public Optional<Relation> findById(Long id) {return relationRepository.findById(id); }
+
+    public Collection<Relation> findByCompany(Long id) {
+        return relationRepository.findByCompany_Id(id);
+    }
+
+    public boolean hasAccess(User user, Relation relation) {
+        if (user.getRole().getRoleType().equals(RoleType.ROLE_SUPER_ADMIN)) {
+            return true;
+        } else return user.getCompany().getId().equals(relation.getChild().getCompany().getId());
+    }
+
+    public boolean canCreate(User user, Relation relationReq) {
+
+        return canPatch(user, modelMapper.map(relationReq, RelationPatchDTO.class));
+    }
+
+    public boolean canPatch(User user, RelationPatchDTO relationReq) {
+        Long companyId = user.getCompany().getId();
+
+        Optional<WorkOrder> optionalParentWorkOrder = relationReq.getParent() == null ? Optional.empty() : workOrderService.findById(relationReq.getParent().getId());
+        Optional<WorkOrder> optionalChildWorkOrder = relationReq.getChild() == null ? Optional.empty() : workOrderService.findById(relationReq.getChild().getId());
+
+        boolean first = !optionalParentWorkOrder.isPresent() || optionalParentWorkOrder.get().getCompany().getId().equals(companyId);
+        boolean second = !optionalChildWorkOrder.isPresent() || optionalChildWorkOrder.get().getCompany().getId().equals(companyId);
+
+        return first && second;
+    }
+
 }
