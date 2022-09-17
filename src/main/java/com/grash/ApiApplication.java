f860ca2b28fcddafa32997d9efd1d184760dc9fc
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-17 20:43:17 +0100

    refactor: ModelMapper in configurations

--- src/main/java/com/grash/ApiApplication.java
+++ src/main/java/com/grash/ApiApplication.java
@@ -4,12 +4,10 @@ import com.grash.model.Role;
 import com.grash.model.enums.RoleType;
 import com.grash.service.RoleService;
 import lombok.RequiredArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -24,13 +22,8 @@ public class ApiApplication implements CommandLineRunner {
         SpringApplication.run(ApiApplication.class, args);
     }
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
-
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         if (!roleService.findByName(superAdminRole).isPresent()) {
             roleService.create(new Role(RoleType.ROLE_SUPER_ADMIN, superAdminRole));
         }
