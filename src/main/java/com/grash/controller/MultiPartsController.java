b334a8903b9bac61627a3a8ab57034169dfbef54
ambou-97 <ambroise.tine@gmail.com>
2022-09-18 16:04:02 +0100

    chore: MultiPartsController

---
+++ src/main/java/com/grash/controller/MultiPartsController.java
@@ -0,0 +1,115 @@
+package com.grash.controller;
+
+import com.grash.dto.MultiPartsPatchDTO;
+import com.grash.dto.SuccessResponse;
+import com.grash.exception.CustomException;
+import com.grash.model.MultiParts;
+import com.grash.model.User;
+import com.grash.model.enums.RoleType;
+import com.grash.service.MultiPartsService;
+import com.grash.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/multiPartss")
+@Api(tags = "multiParts")
+@RequiredArgsConstructor
+public class MultiPartsController {
+
+    private final MultiPartsService multiPartsService;
+    private final UserService userService;
+
+    @GetMapping("")
+    @PreAuthorize("permitAll()")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "MultiPartsCategory not found")})
+    public Collection<MultiParts> getAll(HttpServletRequest req) {
+        User user = userService.whoami(req);
+        if (user.getRole().getRoleType().equals(RoleType.ROLE_CLIENT)) {
+            return multiPartsService.findByCompany(user.getCompany().getId());
+        } else return multiPartsService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("permitAll()")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "MultiParts not found")})
+    public MultiParts getById(@ApiParam("id") @PathVariable("id") Long id, HttpServletRequest req) {
+        User user = userService.whoami(req);
+        Optional<MultiParts> optionalMultiParts = multiPartsService.findById(id);
+        if (optionalMultiParts.isPresent()) {
+            MultiParts savedMultiParts = optionalMultiParts.get();
+            if (multiPartsService.hasAccess(user, savedMultiParts)) {
+                return optionalMultiParts.get();
+            } else throw new CustomException("Access denied", HttpStatus.FORBIDDEN);
+        } else throw new CustomException("Not found", HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("")
+    @PreAuthorize("hasMultiParts('ROLE_CLIENT')")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied")})
+    public MultiParts create(@ApiParam("MultiParts") @RequestBody MultiParts multiPartsReq, HttpServletRequest req) {
+        User user = userService.whoami(req);
+        if (multiPartsService.canCreate(user, multiPartsReq)) {
+            return multiPartsService.create(multiPartsReq);
+        } else throw new CustomException("Access denied", HttpStatus.FORBIDDEN);
+    }
+
+    @PatchMapping("/{id}")
+    @PreAuthorize("hasMultiParts('ROLE_CLIENT')")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied"), //
+            @ApiResponse(code = 404, message = "MultiParts not found")})
+    public MultiParts patch(@ApiParam("MultiParts") @RequestBody MultiPartsPatchDTO multiParts, @ApiParam("id") @PathVariable("id") Long id,
+                            HttpServletRequest req) {
+        User user = userService.whoami(req);
+        Optional<MultiParts> optionalMultiParts = multiPartsService.findById(id);
+
+        if (optionalMultiParts.isPresent()) {
+            MultiParts savedMultiParts = optionalMultiParts.get();
+            if (multiPartsService.hasAccess(user, savedMultiParts) && multiPartsService.canPatch(user, multiParts)) {
+                return multiPartsService.update(id, multiParts);
+            } else throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
+        } else throw new CustomException("MultiParts not found", HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasMultiParts('ROLE_CLIENT')")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied"), //
+            @ApiResponse(code = 404, message = "MultiParts not found")})
+    public ResponseEntity delete(@ApiParam("id") @PathVariable("id") Long id, HttpServletRequest req) {
+        User user = userService.whoami(req);
+
+        Optional<MultiParts> optionalMultiParts = multiPartsService.findById(id);
+        if (optionalMultiParts.isPresent()) {
+            MultiParts savedMultiParts = optionalMultiParts.get();
+            if (multiPartsService.hasAccess(user, savedMultiParts)) {
+                multiPartsService.delete(id);
+                return new ResponseEntity(new SuccessResponse(true, "Deleted successfully"),
+                        HttpStatus.OK);
+            } else throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
+        } else throw new CustomException("MultiParts not found", HttpStatus.NOT_FOUND);
+    }
+
+}
--- src/main/java/com/grash/dto/MultiPartsPatchDTO.java
+++ src/main/java/com/grash/dto/MultiPartsPatchDTO.java
@@ -1,10 +1,14 @@
 package com.grash.dto;
 
+import com.grash.model.Company;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 public class MultiPartsPatchDTO {
+
     private String name;
+
+    private Company company;
 }
