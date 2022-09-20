3939b2607be6858efa48b7e78a7dd4ee38be9251
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-20 12:39:27 +0100

    chore: NotificationController

---
+++ src/main/java/com/grash/controller/NotificationController.java
@@ -0,0 +1,79 @@
+package com.grash.controller;
+
+import com.grash.dto.SuccessResponse;
+import com.grash.exception.CustomException;
+import com.grash.model.Notification;
+import com.grash.model.User;
+import com.grash.model.enums.RoleType;
+import com.grash.service.NotificationService;
+import com.grash.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/notifications")
+@Api(tags = "notification")
+@RequiredArgsConstructor
+public class NotificationController {
+
+    private final NotificationService notificationService;
+    private final UserService userService;
+
+    @GetMapping("")
+    @PreAuthorize("permitAll()")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "NotificationCategory not found")})
+    public Collection<Notification> getAll(HttpServletRequest req) {
+        User user = userService.whoami(req);
+        if (user.getRole().getRoleType().equals(RoleType.ROLE_CLIENT)) {
+            return notificationService.findByUser(user.getId());
+        } else return notificationService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("permitAll()")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Notification not found")})
+    public Notification getById(@ApiParam("id") @PathVariable("id") Long id, HttpServletRequest req) {
+        User user = userService.whoami(req);
+        Optional<Notification> optionalNotification = notificationService.findById(id);
+        if (optionalNotification.isPresent()) {
+            Notification savedNotification = optionalNotification.get();
+            if (notificationService.hasAccess(user, savedNotification)) {
+                return savedNotification;
+            } else throw new CustomException("Access denied", HttpStatus.FORBIDDEN);
+        } else throw new CustomException("Not found", HttpStatus.NOT_FOUND);
+    }
+
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_SUPER_ADMIN')")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied"), //
+            @ApiResponse(code = 404, message = "Notification not found")})
+    public ResponseEntity delete(@ApiParam("id") @PathVariable("id") Long id, HttpServletRequest req) {
+        Optional<Notification> optionalNotification = notificationService.findById(id);
+        if (optionalNotification.isPresent()) {
+            notificationService.delete(id);
+            return new ResponseEntity(new SuccessResponse(true, "Deleted successfully"),
+                    HttpStatus.OK);
+        } else throw new CustomException("Notification not found", HttpStatus.NOT_FOUND);
+    }
+
+}
