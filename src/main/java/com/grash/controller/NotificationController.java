9143c56a628cd055c3f231a508f2dd6f0166858a
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-20 12:47:29 +0100

    feat: Notification patch

--- src/main/java/com/grash/controller/NotificationController.java
+++ src/main/java/com/grash/controller/NotificationController.java
@@ -1,5 +1,6 @@
 package com.grash.controller;
 
+import com.grash.dto.NotificationPatchDTO;
 import com.grash.dto.SuccessResponse;
 import com.grash.exception.CustomException;
 import com.grash.model.Notification;
@@ -18,6 +19,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -60,6 +62,24 @@ public class NotificationController {
         } else throw new CustomException("Not found", HttpStatus.NOT_FOUND);
     }
 
+    @PatchMapping("/{id}")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @ApiResponses(value = {//
+            @ApiResponse(code = 500, message = "Something went wrong"), //
+            @ApiResponse(code = 403, message = "Access denied"), //
+            @ApiResponse(code = 404, message = "Notification not found")})
+    public Notification patch(@ApiParam("Notification") @Valid @RequestBody NotificationPatchDTO notification, @ApiParam("id") @PathVariable("id") Long id,
+                              HttpServletRequest req) {
+        User user = userService.whoami(req);
+        Optional<Notification> optionalNotification = notificationService.findById(id);
+
+        if (optionalNotification.isPresent()) {
+            Notification savedNotification = optionalNotification.get();
+            if (notificationService.hasAccess(user, savedNotification) && notificationService.canPatch(user, notification)) {
+                return notificationService.update(id, notification);
+            } else throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
+        } else throw new CustomException("Notification not found", HttpStatus.NOT_FOUND);
+    }
 
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ROLE_SUPER_ADMIN')")
--- src/main/java/com/grash/service/NotificationService.java
+++ src/main/java/com/grash/service/NotificationService.java
@@ -1,10 +1,14 @@
 package com.grash.service;
 
+import com.grash.dto.NotificationPatchDTO;
+import com.grash.exception.CustomException;
+import com.grash.mapper.NotificationMapper;
 import com.grash.model.Notification;
 import com.grash.model.User;
 import com.grash.model.enums.RoleType;
 import com.grash.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -14,9 +18,17 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class NotificationService {
     private final NotificationRepository notificationRepository;
+    private final NotificationMapper notificationMapper;
 
     public Notification create(Notification Notification) {
         return notificationRepository.save(Notification);
+    }
+
+    public Notification update(Long id, NotificationPatchDTO notificationsPatchDTO) {
+        if (notificationRepository.existsById(id)) {
+            Notification savedNotification = notificationRepository.findById(id).get();
+            return notificationRepository.save(notificationMapper.updateNotification(savedNotification, notificationsPatchDTO));
+        } else throw new CustomException("Not found", HttpStatus.NOT_FOUND);
     }
 
     public Collection<Notification> getAll() {
@@ -39,5 +51,9 @@ public class NotificationService {
         if (user.getRole().getRoleType().equals(RoleType.ROLE_SUPER_ADMIN)) {
             return true;
         } else return user.getId().equals(notification.getUser().getId());
+    }
+
+    public boolean canPatch(User user, NotificationPatchDTO notification) {
+        return true;
     }
 }
