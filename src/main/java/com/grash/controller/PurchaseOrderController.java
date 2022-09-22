d62f9a366f228606852d9adb4b0d2d0b4fff5e6f
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-22 14:13:42 +0100

    fix: PurchaseOrderController

--- src/main/java/com/grash/controller/PurchaseOrderController.java
+++ src/main/java/com/grash/controller/PurchaseOrderController.java
@@ -64,7 +64,7 @@ public class PurchaseOrderController {
     }
 
     @PostMapping("")
-    @PreAuthorize("hasPurchaseOrder('ROLE_CLIENT')")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
     @ApiResponses(value = {//
             @ApiResponse(code = 500, message = "Something went wrong"), //
             @ApiResponse(code = 403, message = "Access denied")})
@@ -76,7 +76,7 @@ public class PurchaseOrderController {
     }
 
     @PatchMapping("/{id}")
-    @PreAuthorize("hasPurchaseOrder('ROLE_CLIENT')")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
     @ApiResponses(value = {//
             @ApiResponse(code = 500, message = "Something went wrong"), //
             @ApiResponse(code = 403, message = "Access denied"), //
@@ -95,7 +95,7 @@ public class PurchaseOrderController {
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasPurchaseOrder('ROLE_CLIENT')")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
     @ApiResponses(value = {//
             @ApiResponse(code = 500, message = "Something went wrong"), //
             @ApiResponse(code = 403, message = "Access denied"), //
