3006816779ac2a18f2919f29be0f51bbcf41b450
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-09-22 14:26:58 +0100

    chore: Relation extends CompanyAudit

--- src/main/java/com/grash/model/Relation.java
+++ src/main/java/com/grash/model/Relation.java
@@ -1,24 +1,29 @@
 package com.grash.model;
 
+import com.grash.model.abstracts.CompanyAudit;
 import com.grash.model.enums.RelationType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
 @NoArgsConstructor
-public class Relation {
+public class Relation extends CompanyAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotNull
     private RelationType relationType = RelationType.RELATED_TO;
 
     @ManyToOne
+    @NotNull
     private WorkOrder parent;
 
+    @NotNull
     @ManyToOne
     private WorkOrder child;
 
