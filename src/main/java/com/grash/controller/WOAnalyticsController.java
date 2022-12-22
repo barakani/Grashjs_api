fba5fb2e0e0deb6d56fd1c0229617f2e2b066911
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2022-12-22 19:11:05 +0100

    feat: incomplete wo by priority

--- src/main/java/com/grash/controller/WOAnalyticsController.java
+++ src/main/java/com/grash/controller/WOAnalyticsController.java
@@ -1,10 +1,13 @@
 package com.grash.controller;
 
 import com.grash.dto.analytics.WOStats;
+import com.grash.dto.analytics.WOStatsByPriority;
 import com.grash.exception.CustomException;
 import com.grash.model.OwnUser;
 import com.grash.model.WorkOrder;
+import com.grash.model.abstracts.WorkOrderBase;
 import com.grash.model.enums.PermissionEntity;
+import com.grash.model.enums.Priority;
 import com.grash.model.enums.Status;
 import com.grash.service.UserService;
 import com.grash.service.WorkOrderService;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
@@ -53,4 +57,54 @@ public class WOAnalyticsController {
         } else throw new CustomException("Access Denied", HttpStatus.FORBIDDEN);
     }
 
+    @GetMapping("/incomplete-priority")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    public WOStatsByPriority getIncompleteByPriority(HttpServletRequest req) {
+        OwnUser user = userService.whoami(req);
+        if (user.getRole().getViewPermissions().contains(PermissionEntity.ANALYTICS)) {
+            Collection<WorkOrder> workOrders = workOrderService.findByCompany(user.getCompany().getId());
+            Collection<WorkOrder> incompleteWO = workOrders.stream().filter(workOrder -> !workOrder.getStatus().equals(Status.COMPLETE)).collect(Collectors.toList());
+
+            List<Integer> highValues = getCountsAndEstimatedDuration(Priority.HIGH, incompleteWO);
+            List<Integer> noneValues = getCountsAndEstimatedDuration(Priority.NONE, incompleteWO);
+            List<Integer> lowValues = getCountsAndEstimatedDuration(Priority.LOW, incompleteWO);
+            List<Integer> mediumValues = getCountsAndEstimatedDuration(Priority.MEDIUM, incompleteWO);
+
+            int highCounts = highValues.get(0);
+            int highEstimatedDurations = highValues.get(1);
+            int mediumCounts = mediumValues.get(0);
+            int mediumEstimatedDurations = mediumValues.get(1);
+            int lowCounts = lowValues.get(0);
+            int lowEstimatedDurations = lowValues.get(1);
+            int noneCounts = noneValues.get(0);
+            int noneEstimatedDurations = noneValues.get(1);
+
+            return WOStatsByPriority.builder()
+                    .high(WOStatsByPriority.BasicStats.builder()
+                            .count(highCounts)
+                            .estimatedHours(highEstimatedDurations)
+                            .build())
+                    .none(WOStatsByPriority.BasicStats.builder()
+                            .count(noneCounts)
+                            .estimatedHours(noneEstimatedDurations)
+                            .build())
+                    .low(WOStatsByPriority.BasicStats.builder()
+                            .count(lowCounts)
+                            .estimatedHours(lowEstimatedDurations)
+                            .build())
+                    .medium(WOStatsByPriority.BasicStats.builder()
+                            .count(mediumCounts)
+                            .estimatedHours(mediumEstimatedDurations)
+                            .build())
+                    .build();
+
+        } else throw new CustomException("Access Denied", HttpStatus.FORBIDDEN);
+    }
+
+    public List<Integer> getCountsAndEstimatedDuration(Priority priority, Collection<WorkOrder> workOrders) {
+        Collection<WorkOrder> priorityWO = workOrders.stream().filter(workOrder -> workOrder.getPriority().equals(priority)).collect(Collectors.toList());
+        int priorityCounts = priorityWO.size();
+        int priorityEstimatedDurations = priorityWO.stream().map(WorkOrderBase::getEstimatedDuration).mapToInt(value -> value).sum();
+        return Arrays.asList(priorityCounts, priorityEstimatedDurations);
+    }
 }
