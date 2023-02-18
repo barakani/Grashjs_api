80d146dcad4f535338d774bcf8f5776343a6cb56
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2023-02-18 17:45:38 +0100

    refactor: downgrade and upgrade in subscription controller

--- src/main/java/com/grash/controller/SubscriptionController.java
+++ src/main/java/com/grash/controller/SubscriptionController.java
@@ -1,5 +1,8 @@
 package com.grash.controller;
 
+import com.grash.dto.SuccessResponse;
+import com.grash.exception.CustomException;
+import com.grash.model.OwnUser;
 import com.grash.model.Subscription;
 import com.grash.service.SubscriptionService;
 import com.grash.service.UserService;
@@ -7,13 +10,13 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/subscriptions")
@@ -35,7 +38,7 @@ public class SubscriptionController {
     }
 
 
-//    @DeleteMapping("/{id}")
+    //    @DeleteMapping("/{id}")
 //    @PreAuthorize("hasRole('ROLE_CLIENT')")
 //    @ApiResponses(value = {//
 //            @ApiResponse(code = 500, message = "Something went wrong"), //
@@ -54,5 +57,49 @@ public class SubscriptionController {
 //            } else throw new CustomException("Forbidden", HttpStatus.FORBIDDEN);
 //        } else throw new CustomException("Subscription not found", HttpStatus.NOT_FOUND);
 //    }
+    @PostMapping("/upgrade")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    public SuccessResponse upgrade(@RequestBody Collection<Long> usersIds,
+                                   HttpServletRequest req) {
+        OwnUser user = userService.whoami(req);
+        if (user.isOwnsCompany()) {
+            int enabledUsersCount = (int) userService.findByCompany(user.getCompany().getId()).stream().filter(OwnUser::isEnabledInSubscription).count();
+            Subscription subscription = user.getCompany().getSubscription();
+            int subscriptionUsersCount = subscription.getUsersCount();
+            if (enabledUsersCount + usersIds.size() <= subscriptionUsersCount) {
+                Collection<OwnUser> users = usersIds.stream().map(userId -> userService.findById(userId).get()).collect(Collectors.toList());
+                if (users.stream().noneMatch(OwnUser::isEnabledInSubscription)) {
+                    users.forEach(user1 -> user1.setEnabledInSubscription(true));
+                    userService.saveAll(users);
+                    subscription.setUpgradeNeeded(false);
+                    subscriptionService.save(subscription);
+                    return new SuccessResponse(true, "Users enabled successfully");
+                } else throw new CustomException("There are some already enabled users", HttpStatus.NOT_ACCEPTABLE);
+            } else
+                throw new CustomException("The subscription users count doesn't permit this operation", HttpStatus.NOT_ACCEPTABLE);
+        } else throw new CustomException("Access Denied", HttpStatus.FORBIDDEN);
+    }
 
+    @PostMapping("/downgrade")
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    public SuccessResponse downgrade(@RequestBody Collection<Long> usersIds,
+                                     HttpServletRequest req) {
+        OwnUser user = userService.whoami(req);
+        if (user.isOwnsCompany()) {
+            int enabledUsersCount = (int) userService.findByCompany(user.getCompany().getId()).stream().filter(OwnUser::isEnabledInSubscription).count();
+            Subscription subscription = user.getCompany().getSubscription();
+            int subscriptionUsersCount = user.getCompany().getSubscription().getUsersCount();
+            if (enabledUsersCount - usersIds.size() <= subscriptionUsersCount) {
+                Collection<OwnUser> users = usersIds.stream().map(userId -> userService.findById(userId).get()).collect(Collectors.toList());
+                if (users.stream().allMatch(OwnUser::isEnabledInSubscription)) {
+                    users.forEach(user1 -> user1.setEnabledInSubscription(false));
+                    userService.saveAll(users);
+                    subscription.setDowngradeNeeded(false);
+                    subscriptionService.save(subscription);
+                    return new SuccessResponse(true, "Users enabled successfully");
+                } else throw new CustomException("There are some already disabled users", HttpStatus.NOT_ACCEPTABLE);
+            } else
+                throw new CustomException("The subscription users count doesn't permit this operation", HttpStatus.NOT_ACCEPTABLE);
+        } else throw new CustomException("Access Denied", HttpStatus.FORBIDDEN);
+    }
 }
