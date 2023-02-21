da0580f09506f15ff07808da27960eab2d51a815
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2023-02-21 21:39:53 +0100

    fix: subscription charge

--- src/main/java/com/grash/dto/fastSpring/payloads/SubscriptionCharge.java
+++ src/main/java/com/grash/dto/fastSpring/payloads/SubscriptionCharge.java
@@ -1,12 +1,10 @@
 package com.grash.dto.fastSpring.payloads;
 
-import com.grash.dto.fastSpring.Account;
 import com.grash.dto.fastSpring.Subscription;
 import lombok.Data;
 
 @Data
 public class SubscriptionCharge {
-    public Order order;
     public String currency;
     public int total;
     public String status;
@@ -16,7 +14,6 @@ public class SubscriptionCharge {
     public String timestampDisplay;
     public int sequence;
     public Integer periods;
-    public Account account;
     public String quote;
     public Subscription subscription;
 }
