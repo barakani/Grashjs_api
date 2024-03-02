f1e42d1a41afa17ff4c1ca6edb6a995f1d74bb3f
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2024-03-02 01:49:04 +0100

    fix: CompanyAudit

--- src/main/java/com/grash/model/abstracts/CompanyAudit.java
+++ src/main/java/com/grash/model/abstracts/CompanyAudit.java
@@ -44,7 +44,9 @@ public class CompanyAudit extends Audit {
         OwnUser user = ((CustomUserDetail) principal).getUser();
         Company company = user.getCompany();
         // check if not authorized
-        if (!user.getRole().getRoleType().equals(RoleType.ROLE_SUPER_ADMIN) && !company.getId().equals(this.getCompany().getId())) {
+        if (!user.getRole().getRoleType().equals(RoleType.ROLE_SUPER_ADMIN) &&
+                !company.getId().equals(this.getCompany().getId())
+                && !makesException(user)) {
             throw new CustomException("afterLoad:  the user (username=" + user.getUsername() + ")  is not authorized to load  this object (" + this.getClass() + ") with id" + this.id, HttpStatus.FORBIDDEN);
         }
     }
@@ -52,7 +54,7 @@ public class CompanyAudit extends Audit {
     private boolean makesException(OwnUser user) {
         if (this instanceof File) {
             return user.getSuperAccountRelations().stream()
-                    .anyMatch(relation->relation.getChildUser().getCompany().getId().equals(this.company.getId()));
+                    .anyMatch(relation -> relation.getChildUser().getCompany().getId().equals(this.company.getId()));
         }
         return false;
     }
