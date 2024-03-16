564f02c97b8e5090a3f015726c75659d035d3c40
Ibrahima G. Coulibaly <ibracool99@gmail.com>
2024-03-16 02:26:31 +0000

    fix: query @Param

--- src/main/java/com/grash/repository/WorkOrderAudRepository.java
+++ src/main/java/com/grash/repository/WorkOrderAudRepository.java
@@ -4,11 +4,12 @@ import com.grash.model.AdditionalCost;
 import com.grash.model.envers.WorkOrderAud;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Collection;
 import java.util.List;
 
 public interface WorkOrderAudRepository extends JpaRepository<WorkOrderAud, Long> {
     @Query("SELECT w FROM WorkOrderAud w WHERE w.workOrderAudId.id = :id AND w.revtype= :revType")
-    List<WorkOrderAud> findByIdAndRevtype(Long id, Integer revType );
+    List<WorkOrderAud> findByIdAndRevtype(@Param("id")Long id, @Param("revType") Integer revType );
 }
